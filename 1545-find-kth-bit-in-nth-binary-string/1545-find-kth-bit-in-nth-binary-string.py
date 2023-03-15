e1ac25cdc8c478b7a951c95e31fe9f695e3e5115
TAD <tadiyosbegashaw12@gmail.com>
2023-03-15 20:25:01 +0300

    Time: 850 ms (44.34%), Space: 26.1 MB (27.59%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,5 +1,6 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
+        self.past = {}
         def reverse(x):
             return x[::-1]
         def invert(x):
@@ -13,7 +14,10 @@ class Solution:
         def rec(n):
             if n == 1:
                 return "0"
-            return rec(n-1) + "1" + reverse(invert(rec(n-1)))
+            if n in self.past:
+                return self.past[n]
+            self.past[n] = rec(n-1) + "1" + reverse(invert(rec(n-1)))
+            return self.past[n]
         res = rec(n)
         return res[k-1]
         
