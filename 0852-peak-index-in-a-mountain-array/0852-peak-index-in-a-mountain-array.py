358597a09f106477407e368b5b6ea341e01de78f
TAD <tadiyosbegashaw12@gmail.com>
2023-03-24 20:28:25 +0300

    Time: 570 ms (99.72%), Space: 27.2 MB (93.03%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -3,11 +3,12 @@ class Solution:
         l, r = 0 , len(arr)-1
         while l <= r:
             mid = l + (r-l)//2
-            if arr[mid-1] < arr[mid] and arr[mid+1] > arr[mid]:
-                l = mid
-            elif arr[mid-1] > arr[mid] and arr[mid+1] < arr[mid]:
-                r = mid
-            elif arr[mid-1] < arr[mid] and arr[mid+1] < arr[mid]:
+            if arr[mid-1] < arr[mid] and arr[mid+1] < arr[mid]:
                 return mid
+            elif arr[mid-1] < arr[mid]:
+                l = mid
+            elif arr[mid-1] > arr[mid]:
+                r = mid
+            
         return l
         
