07e2ac58bab1965d0f5b87c1b613e729b25f02c3
TAD <tadiyosbegashaw12@gmail.com>
2023-11-17 11:00:02 +0300

    Time: 40 ms (70.47%), Space: 16.1 MB (86.06%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,25 +1,44 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        collect = set()
-        
-        for item in nums:
-            tmp = int(item,2)
-            collect.add(tmp)
-            
-        #print(collect) 
-        n = pow(2,len(nums))
-        
-        for i in range(n):
-            if i not in collect:
+        result = set()
+        for num in nums:
+            integer = int(num,2)
+            result.add(integer)
+        the_max_number = pow(2,len(nums))
+        for i in range(the_max_number):
+            if i not  in result:
                 ans = bin(i)
                 ans = ans[2:]
-                leng = len(nums[0]) - len(ans)
+            #ans = 01
+            #len(nums) = 001
+                leng = len(nums) - len(ans)
                 tmp = ""
                 while leng > 0:
                     tmp += "0"
-                    leng -= 1
+                    leng -=1  
                 ans = tmp + ans
                 return ans
+            
+#         collect = set()
+        
+#         for item in nums:
+#             tmp = int(item,2)
+#             collect.add(tmp)
+            
+#         #print(collect) 
+#         n = pow(2,len(nums))
+        
+#         for i in range(n):
+#             if i not in collect:
+#                 ans = bin(i)
+#                 ans = ans[2:]
+#                 leng = len(nums[0]) - len(ans)
+#                 tmp = ""
+#                 while leng > 0:
+#                     tmp += "0"
+#                     leng -= 1
+#                 ans = tmp + ans
+#                 return ans
             
             
         
